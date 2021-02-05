72813e730571c4c25c0bbfc853bfb45e3d0dc086
saadsawash <saa-d@outlook.com>
2021-02-05 22:49:39 +0300

    Built the SDG feature

--- assets/scripts/kokar.js
+++ assets/scripts/kokar.js
@@ -48,7 +48,85 @@ $( document ).ready(function() {
 	$carousel.on( 'select.flickity', function(event, index) {
 		$.get('../events.html', function(html) {
 			var events = $('<section />', {html: html}).find('.sdg' + (index + 1));
+			// Get The Number Of Events The Current SDG Has
 			var numberOfEvents = events.length;
+			// Print The Event Count At Its Appropriate Place
+			$( ".event-count-span" ).text(numberOfEvents);
+			// Check Which SDG Is Selected To Display Its Information
+			var SDGName = "";
+			var SDGDesc = "";
+			switch ((index + 1)) {
+				case 1:
+					SDGName = "No Poverty";
+					SDGDesc = "End poverty in all its forms everywhere.";
+					break;
+				case 2:
+					SDGName = "Zero Hunger";
+					SDGDesc = "End hunger, achieve food security and improved nutrition and promote sustainable agriculture.";
+					break;
+				case 3:
+					SDGName = "Good Health and Well-Being";
+					SDGDesc = "Ensure healthy lives and promote well-being for all at all ages.";
+					break;
+				case 4:
+					SDGName = "Quality Education";
+					SDGDesc = "Ensure inclusive and equitable quality education and promote lifelong learning opportunities for all.";
+					break;
+				case 5:
+					SDGName = "Gender Equality";
+					SDGDesc = "Achieve gender equality and empower all women and girls.";
+					break;
+				case 6:
+					SDGName = "Clean Water and Sanitation";
+					SDGDesc = "Ensure availability and sustainable management of water and sanitation for all.";
+					break;
+				case 7:
+					SDGName = "Affordable and Clean Energy";
+					SDGDesc = "Ensure access to affordable, reliable, sustainable and modern energy for all.";
+					break;
+				case 8:
+					SDGName = "Decent Work and Economic Growth";
+					SDGDesc = "Promote sustained, inclusive and sustainable economic growth, full and productive employment and decent work for all.";
+					break;
+				case 9:
+					SDGName = "Industry, Innovation and Infrustructure";
+					SDGDesc = "Build resilient infrastructure, promote inclusive and sustainable industrialization and foster innovation.";
+					break;
+				case 10:
+					SDGName = "Reduced Inequalities";
+					SDGDesc = "Reduce inequality within and among countries.";
+					break;
+				case 11:
+					SDGName = "Sustainable Cities and Communities";
+					SDGDesc = "Make cities and human settlements inclusive, safe, resilient and sustainable.";
+					break;
+				case 12:
+					SDGName = "Responsible Consumption and Production";
+					SDGDesc = "Ensure sustainable consumption and production patterns.";
+					break;
+				case 13:
+					SDGName = "Climate Action";
+					SDGDesc = "Take urgent action to combat climate change and its impacts.";
+					break;
+				case 14:
+					SDGName = "Life Below Water";
+					SDGDesc = "Conserve and sustainably use the oceans, seas and marine resources for sustainable development.";
+					break;
+				case 15:
+					SDGName = "Life on Land";
+					SDGDesc = "Protect, restore and promote sustainable use of terrestrial ecosystems, sustainably manage forests, combat desertification, and halt and reverse land degradation and halt biodiversity loss.";
+				case 16:
+					SDGName = "Peace, Justice and Strong Institutions";
+					SDGDesc = "Promote peaceful and inclusive societies for sustainable development, provide access to justice for all and build effective, accountable and inclusive institutions at all levels.";
+					break;
+				case 17:
+					SDGName = "Partnerships for the Goals";
+					SDGDesc = "Strengthen the means of implementation and revitalize the global partnership for sustainable development.";					
+					break;
+			}
+			$( ".info_a h2" ).text((index + 1) + " " + SDGName);
+			$( ".info_a p" ).text(SDGDesc);
+
 			if (numberOfEvents == 0) {
 				$('.no-event').show();
 			} else {
